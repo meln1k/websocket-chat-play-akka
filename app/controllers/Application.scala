59df46c8bf6400e1da0d16717faf4bda904928b2
Nikita Melkozerov <n.melkozerov@gmail.com>
2014-11-15 15:56:07 +0500

    websockets test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
+import actors._
+
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+
+  def socket = WebSocket.acceptWithActor[String, String] { request => out =>
+    MyWebSocketActor.props(out)
   }
 
 }
