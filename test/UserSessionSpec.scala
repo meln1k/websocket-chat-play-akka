f9bec1306ad757714f8d7acd07572ce1e839ce4a
Nikita Melkozerov <n.melkozerov@gmail.com>
2014-12-13 20:31:41 +0500

    test refactoring

--- test/UserSessionSpec.scala
+++ test/UserSessionSpec.scala
@@ -2,7 +2,7 @@ import actors.Messages._
 import actors.UserSession
 import actors.Constants._
 import akka.actor.ActorSystem
-import akka.testkit.{TestKit, TestActorRef, ImplicitSender}
+import akka.testkit.{TestKit, TestActorRef}
 import org.scalatest._
 import scala.concurrent.duration._
 
@@ -30,18 +30,21 @@ class UserSessionSpec extends TestKit(ActorSystem("UserSessionSpec"))
       testA ! InMsg(SendMessage, text)
       expectMsg(ChatMessage(name, text))
     }
-    "don't send user message to the room if name isn't registered" in {
+
+    "not send user message to the room if name isn't registered" in {
       val testA = TestActorRef[UserSession](UserSession.props(testActor, testActor))
       val text = util.Random.nextString(10)
       testA.underlyingActor.userName = None
       testA ! InMsg(SendMessage, text)
       expectNoMsg(50 milliseconds)
     }
+
     "signal when invite denied" in {
       val testA = TestActorRef[UserSession](UserSession.props(testActor, testActor))
       testA ! InviteDenied
       expectMsgPF(hint = """OutMsg(_, "system", "taken")"""){case OutMsg(_, "system", "taken")=>}
     }
+
     "send chat message to user" in {
       val testA = TestActorRef[UserSession](UserSession.props(testActor, testActor))
       val RandomText = util.Random.nextString(10)
@@ -49,6 +52,7 @@ class UserSessionSpec extends TestKit(ActorSystem("UserSessionSpec"))
       testA ! ChatMessage(RandomText, RandomText)
       expectMsgPF(hint = s"""OutMsg(_, "$RandomText", "$RandomText")"""){case OutMsg(_, RandomText, RandomText)=>}
     }
+
     "send broadcast message to user" in {
       val testA = TestActorRef[UserSession](UserSession.props(testActor, testActor))
       val RandomText = util.Random.nextString(10)
